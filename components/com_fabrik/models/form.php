jaanusnurmoja/fabrikforj4

commit f4c33b29fafce139b8a59dfba184e1162db7538c

Need to set _editable in render() in form model, so plugins that run on load, contentTop have that setting, so added a call to getAccessFromListSettings(), or whatever it's called.

components/com_fabrik/models/form.php
@@ -2929,6 +2929,8 @@ INNER JOIN #__{package}_groups as g ON g.id = fg.group_id
 		$this->_listModel = null;
 		@set_time_limit(300);
 		$this->_rowId = $this->getRowId();
+		// $$$ hugh - need to call this here as we set $this->_editable here, which is needed by some plugins
+		$this->checkAccessFromListSettings();
 		$pluginManager = FabrikWorker::getPluginManager();
 		$res = $pluginManager->runPlugins('onBeforeLoad', $this);
 		if (in_array(false, $res))

commit f8a5b22dc4eba75d25cab9a4b9b8091765ad195b

fixed: security issue introduced in f4c33b29fa. Closes GH-488

components/com_fabrik/models/form.php
@@ -323,7 +323,9 @@ class FabrikFEModelForm extends FabModelForm
 	}
 
 	/**
-	 * Makes sure that the form is not viewable based on the table's access settings
+	 * Makes sure that the form is not viewable based on the list's access settings
+	 *
+	 * Also sets the form's editable state, if it can record in to a db table
 	 *
 	 * @return  int  0 = no access, 1 = view only , 2 = full form view, 3 = add record only
 	 */
@@ -346,10 +348,11 @@ class FabrikFEModelForm extends FabModelForm
 			$ret = 1;
 		}
 		$pRowid = FabrikWorker::getMenuOrRequestVar('rowid', '', $this->isMambot);
-		/* new form can we add?*/
+
+		// New form can we add?
 		if ($this->_rowId == 0 || $pRowid == '-1')
 		{
-			/*if they can edit can they also add?*/
+			// If they can edit can they also add
 			if ($listModel->canAdd())
 			{
 				$ret = 3;
@@ -362,14 +365,14 @@ class FabrikFEModelForm extends FabModelForm
 		}
 		else
 		{
-			/*editing from - can we edit?*/
+			// Editing from - can we edit
 			if ($listModel->canEdit($this->_data))
 			{
 				$ret = 2;
 			}
 		}
-		// $$$rob refractored from view
-		$editable = ($ret == 1 && $this->isEditable() == '1') ? false : true;
+		// If no access (0) or read only access (1) set the form to not be editable
+		$editable = ($ret <= 1) ? false : true;
 		$this->setEditable($editable);
 		if (JRequest::getVar('view', 'form') == 'details')
 		{