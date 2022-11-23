ff95e0df75c3d2ea06fb4c3585baf400bb5be445
Alex Chartier <achartier@admiralsecure.com>
2022-11-23 06:37:45 -0500

    Added create table to the update file. The ALTER TABLE IF EXISTS is not supported, so just go ahead and create it.

--- plugins/fabrik_element/sequence/sql/updates/mysql/4.0.sql
+++ plugins/fabrik_element/sequence/sql/updates/mysql/4.0.sql
@@ -1,5 +1,13 @@
-ALTER TABLE IF EXISTS `#__fabrik_sequences` ALTER `table_name` SET DEFAULT '';
-ALTER TABLE IF EXISTS `#__fabrik_sequences` ALTER `affix` SET DEFAULT '';
-ALTER TABLE IF EXISTS `#__fabrik_sequences` ALTER `sequence` SET DEFAULT 0;
-ALTER TABLE IF EXISTS `#__fabrik_sequences` ALTER `element_id` SET DEFAULT 0;
-ALTER TABLE IF EXISTS `#__fabrik_sequences` MODIFY `date_created` datetime NULL DEFAULT NULL;
+CREATE TABLE IF NOT EXISTS  `#__fabrik_sequences` (
+	`table_name` VARCHAR( 64 ) NOT NULL DEFAULT '',
+	`affix` VARCHAR( 32 ) NOT NULL DEFAULT '',
+	`sequence` INT( 6 ) NOT NULL DEFAULT 0,
+	`date_created` DATETIME NULL DEFAULT NULL,
+	`element_id` INT( 6 ) NOT NULL DEFAULT 0,
+	 PRIMARY KEY ( `table_name` , `affix`, `sequence`, `element_id` )
+);
+ALTER TABLE `#__fabrik_sequences` ALTER `table_name` SET DEFAULT '';
+ALTER TABLE `#__fabrik_sequences` ALTER `affix` SET DEFAULT '';
+ALTER TABLE `#__fabrik_sequences` ALTER `sequence` SET DEFAULT 0;
+ALTER TABLE `#__fabrik_sequences` ALTER `element_id` SET DEFAULT 0;
+ALTER TABLE `#__fabrik_sequences` MODIFY `date_created` datetime NULL DEFAULT NULL;
