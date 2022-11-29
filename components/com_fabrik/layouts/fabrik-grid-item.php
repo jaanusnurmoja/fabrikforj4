5959ce64b092ceeb77508f0d6e265962324e4384
Alex Chartier <achartier@admiralsecure.com>
2022-11-29 09:13:29 -0500

    BS5 for radio button groups

--- components/com_fabrik/layouts/fabrik-grid-item.php
+++ components/com_fabrik/layouts/fabrik-grid-item.php
@@ -13,15 +13,16 @@ $thisId = rtrim($thisId, '_');
 $thisId .=  '_input_' . $d->i;
 
 $labelSpan    = '<span>' . $d->label . '</span>';
-$labelClass = '';
-$inputClass = '';
+$labelClass = $d->buttonGroup ? 'btn '.$d->classes : 'form-check-label';
+$inputClass = $d->buttonGroup ? 'btn-check' : 'form-check-input';
+
 
 if (array_key_exists('input', $d->classes))
 {
 	$inputClass .= ' ' . implode(' ', $d->classes['input']);
 }
 
-$chx = '<input type="' . $d->type . '" class="form-check-input fabrikinput ' . $inputClass . '" ' . $d->inputDataAttributes .
+$chx = '<input type="' . $d->type . '" class="fabrikinput ' . $inputClass . '" ' . $d->inputDataAttributes .
 	' name="' . $thisName . '" id="' . $thisId . '" value="' . $d->value . '" ';
 
 $sel = in_array($d->value, $d->selected);
@@ -31,7 +32,7 @@ if (array_key_exists('label', $d->classes))
 {
 	$labelClass .= ' ' . implode(' ', $d->classes['label']);
 }
-$label = '<label for="'.$thisId.'" class="form-check-label fabrikgrid_'.FabrikString::clean($d->value).'">'.$labelSpan.'</label>';
+$label = '<label for="'.$thisId.'" class="'.$labelClass.' fabrikgrid_'.FabrikString::clean($d->value).'">'.$labelSpan.'</label>';
 $html = $d->elementBeforeLabel == '1' ? $chx . $label : $label . $chx; 
 ?>
 
