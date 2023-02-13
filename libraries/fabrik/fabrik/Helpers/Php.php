79cf720da302199e2dbdca5950420d6fbe220ae5
Alex Chartier <achartier@admiralsecure.com>
2023-02-13 06:41:25 -0500

    Revised to build a class instead of a simple function

--- libraries/fabrik/fabrik/Helpers/Php.php
+++ libraries/fabrik/fabrik/Helpers/Php.php
@@ -1,224 +1,162 @@
 <?php
-    /**
-     * @package         Regular Labs Library
-     * @version         22.10.1331
-     *
-     * @author          Peter van Westen <info@regularlabs.com>
-     * @link            http://regularlabs.com
-     * @copyright       Copyright © 2022 Regular Labs All Rights Reserved
-     * @license         http://www.gnu.org/licenses/gpl-2.0.html GNU/GPL
-     */
-    
-    /**
-     * This is a derivative of the Php class from Peter van Weston at Regular Labs,
-     * it is highly modified for Fabrik's needs but the underlying
-     * architecture is copyright Peter van Westen. The following applies to
-     * the derivations.
-     *
-     * @package     Joomla
-     * @subpackage  Fabrik
-     * @copyright   Copyright (C) 2005-2020  Media A-Team, Inc. - All rights reserved.
-     * @license     GNU/GPL http://www.gnu.org/copyleft/gpl.html
-     */
-    
-    namespace Fabrik\Helpers;
-    
-    use Joomla\CMS\Factory;
-    use Joomla\CMS\Version;
-    use Joomla\CMS\Document\Document;
-    use Joomla\CMS\Filesystem\File;
-    
-    class Php
+/**
+ * @package         Regular Labs Library
+ * @version         22.10.1331
+ *
+ * @author          Peter van Westen <info@regularlabs.com>
+ * @link            http://regularlabs.com
+ * @copyright       Copyright © 2022 Regular Labs All Rights Reserved
+ * @license         http://www.gnu.org/licenses/gpl-2.0.html GNU/GPL
+ */
+
+/**
+ * This is a derivative of the Php class from Peter van Weston at Regular Labs,
+ * it is highly modified for Fabrik's needs but the underlying
+ * architecture is copyright Peter van Westen. The following applies to
+ * the derivations.
+ *
+ * @package     Joomla
+ * @subpackage  Fabrik
+ * @copyright   Copyright (C) 2005-2020  Media A-Team, Inc. - All rights reserved.
+ * @license     GNU/GPL http://www.gnu.org/copyleft/gpl.html
+ */
+
+namespace Fabrik\Helpers;
+
+use Joomla\CMS\Factory;
+use Joomla\CMS\Version;
+use Joomla\CMS\Document\Document;
+use Joomla\CMS\Filesystem\File;
+
+class Php
+{
+
+    public static function Eval($params = [])
     {
-        
-        
-        public static function Eval($params = [])
-        {
-            if (empty($params)) {
-                return null;
-            }
-            
-            $codeString = "";
-            
-            if (array_key_exists('file', $params)) {
-                /* We have a php file to include */
-                /* It should be first in the code string */
-            }
-            
-            if (array_key_exists('code', $params)) {
-                /* we have an eval code block */
-                $codeString .= $params['code'];
-            }
-            
-            /* There has to be some code or why are we here */
-            if (empty($codeString)) {
-                return null;
-            }
-            
-            $function_name = self::getFunctionName($codeString);
-            if (!$function_name) {
-                // Something went wrong!
-                return true;
-            }
-            
-            ob_start();
-            $result = $function_name($params['vars'] ?? []);
-            $output = ob_get_contents();
-            ob_end_clean();
-            
-            if ($params['singleReturn'] ?? false) {
-                return $result;
-            }
-            
-            if (!is_array($output)) {
-                return $output;
-            }
-            
-            return $output;
+        if (empty($params)) {
+            return null;
+        }
+
+       
+        if (array_key_exists('code', $params) === false) {
+            /* we must have some code to deal with */
+            return null;
         }
         
-        public
-        static function getApplication()
-        {
-            if (Factory::getApplication()->input->get('option', '') != 'com_finder') {
-                return Factory::getApplication();
-            }
-            
-            return Factory::getApplication()->getInstance('site');
+        $params['className'] = self::getClassName($params);
+        if (empty($params['className'])) {
+            // Something went wrong!
+            return true;
         }
         
-        public
-        static function getDocument()
-        {
-            if (Factory::getApplication()->input->get('option', '') != 'com_finder') {
-                return Document::getInstance();
-            }
-            
-            $lang = Factory::getLanguage();
-            $version = new Version;
-            
-            $attributes = [
-                'charset' => 'utf-8',
-                'lineend' => 'unix',
-                'tab' => "\t",
-                'language' => $lang->getTag(),
-                'direction' => $lang->isRtl() ? 'rtl' : 'ltr',
-                'mediaversion' => $version->getMediaVersion(),
-            ];
-            
-            return Document::getInstance('html', $attributes);
+        ob_start();
+        $newClass = new $params['className'];
+        $result = $newClass->execute();
+        $output = ob_get_contents();
+        ob_end_clean();
+        
+        if (!empty($result)) {
+            return $result;
         }
         
-        private
-        static function createFunctionInMemory(
-            $string
-        ) {
-            $file_name = getmypid() . '_' . md5($string);
-            
-            $tmp_path = Factory::getApplication()->get('tmp_path', JPATH_ROOT . '/tmp');
-            $temp_file = $tmp_path . '/regularlabs' . '/' . $file_name;
-            
-            // Write file
-            if (!file_exists($temp_file) || is_writable($temp_file)) {
-                File::write($temp_file, $string);
-            }
-            
-            // Include file
-            include_once $temp_file;
-            
-            // Delete file
-            if (!Factory::getApplication()->get('debug')) {
-                @chmod($temp_file, 0777);
-                @unlink($temp_file);
-            }
+        return $output;
+    }
+    
+    
+    private static function createFunctionInMemory($string) 
+    {
+        $file_name = getmypid() . '_' . md5($string);
+        
+        $tmp_path = Factory::getApplication()->get('tmp_path', JPATH_ROOT . '/tmp');
+        $temp_file = $tmp_path . '/fabrik' . '/' . $file_name;
+        
+        // Write file
+        if (!file_exists($temp_file) || is_writable($temp_file)) {
+            File::write($temp_file, $string);
         }
         
-        private
-        static function generateFileContents(
-            $function_name = 'rl_function',
-            $string = ''
-        ) {
-            $init_variables = self::getVarInits();
-            
-            $init_variables[] =
-                'if (is_array($rl_variables)) {'
-                . 'foreach ($rl_variables as $rl_key => $rl_value) {'
-                . '${$rl_key} = $rl_value;'
-                . '}'
-                . '}';
-            
-            $contents = [
-                '<?php',
-                'defined(\'_JEXEC\') or die;',
-                'function ' . $function_name . '($rl_variables){',
-                implode("\n", $init_variables),
-                $string . ';',
-                'return get_defined_vars();',
-                ';}',
-            ];
-            
-            $contents = implode("\n", $contents);
-            
-            // Remove Zero Width spaces / (non-)joiners
-            $contents = str_replace(
-                [
-                    "\xE2\x80\x8B",
-                    "\xE2\x80\x8C",
-                    "\xE2\x80\x8D",
-                ],
-                '',
-                $contents
-            );
-            
-            return $contents;
-        }
+        // Include file
+        include_once $temp_file;
         
-        private
-        static function getFunctionName(
-            $string
-        ) {
-            $function_name = 'regularlabs_php_' . md5($string);
-            
-            if (function_exists($function_name)) {
-                return $function_name;
-            }
-            
-            $contents = self::generateFileContents($function_name, $string);
-            self::createFunctionInMemory($contents);
-            
-            if (!function_exists($function_name)) {
-                // Something went wrong!
-                return false;
-            }
-            
-            return $function_name;
-        }
-        
-        private
-        static function getVarInits()
-        {
-            return [
-                '$app = $mainframe = Fabrik\Helpers\Php::getApplication();',
-                '$document = $doc = Fabrik\Helpers\Php::getDocument();',
-                '$database = $db = Joomla\CMS\Factory::getDbo();',
-                '$user = $app->getIdentity() ?: Joomla\CMS\Factory::getUser();',
-                '$Itemid = $app->input->getInt(\'Itemid\');',
-            ];
-        }
-        
-        private
-        static function prepareString(
-            &$string
-        ) {
-            $string = trim($string);
-            $string = str_replace('?><?php', '', $string . '<?php ;');
-            
-            if (substr($string, 0, 5) !== '<?php') {
-                $string = '?>' . $string;
-                
-                return;
-            }
-            
-            $string = substr($string, 5);
+        // Delete file
+        if (!Factory::getApplication()->get('debug')) {
+            @chmod($temp_file, 0777);
+            @unlink($temp_file);
         }
     }
+    
+    private static function generateClassContents($params)
+     {
+        /* Process any $thisVars */
+        $thisVars = $params['thisVars'] ?? [];
+        $privateThisVars = [];
+        $initThisVars = [];
+        foreach($thisVars as $thisVarName => $thisvarSource) {
+            $privateThisVars[] = "private $thisVarName;";
+            $initThisVars[] = "self::$thisVarName = &$thisvarSource"
+        }
+
+        if (count($thisVars)) {
+            array_unshift($initThisVars, "public function __construct() {");
+            $initThisVars[] = "};";
+        }
+
+        /* Process basic vars */
+        $vars = $params['vars'] ?? [];
+        $initBasicVars = [];
+        foreach($vars as $varName => $varSource) {
+            $initBasicVars = "$varName = &varSource";
+        }
+
+        $contents = [
+            '<?php',                            /* Opening stuff  */
+            'defined(\'_JEXEC\') or die;',
+            'class '.$params['className'].'{',            /* Define the class */
+            implode("\n", $privateThisVars),    /* Include any $thisVars definitions */
+            'function doExecute(',              /* Our new function */
+            implode(",", $initBasicVars),       /* Insert our basic variables */
+            ') {',                              /* Close off the function call */
+            $params['code'] . ';'                       /* Now the actual code */
+        ];
+        /* If there are thisVars, we need to add the construct function to initialize them */
+        if (count($thisVars)) {
+            $contents = array_merge($contents, $initThisVars);
+        }
+        
+        $contents[] = "};";     /* And close off the class */
+
+        $contents = implode("\n", $contents);
+        
+        // Remove Zero Width spaces / (non-)joiners
+        $contents = str_replace(
+            [
+                "\xE2\x80\x8B",
+                "\xE2\x80\x8C",
+                "\xE2\x80\x8D",
+            ],
+            '',
+            $contents
+        );
+        
+        return $contents;
+    }
+    
+    private static function getClassName($params) 
+    {
+        $params['className'] = 'FabrikEvalClass_' . md5($params['code']);
+        
+        if (class_exists($params['className'])) {
+            return $params['className'];
+        }
+
+        $contents = self::generateFileContents($params);
+        self::createFunctionInMemory($contents);
+        
+        if (!class_exists($params['className'])) {
+            // Something went wrong!
+            return false;
+        }
+        
+        return $className;
+    }
+}
