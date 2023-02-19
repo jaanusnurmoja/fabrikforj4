fcbd3852283db5f0afba32f67cfa7d579136ed32
Alex Chartier <achartier@admiralsecure.com>
2023-02-19 08:53:57 -0500

    Add the displayData and form to the Eval variables.

--- administrator/components/com_fabrik/overrides/joomla/edit/params.php
+++ administrator/components/com_fabrik/overrides/joomla/edit/params.php
@@ -27,4 +27,4 @@ foreach ($targets as $key=>$target) {
 	}
 }
 
-echo Php::Eval(['code' => '?>'.$buffer.PHP_EOL]);
+echo Php::Eval(['code' => '?>'.$buffer.PHP_EOL, 'vars' => ['displayData'=>$displayData, 'form'=>$form]]);
