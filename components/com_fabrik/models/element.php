d0dd4227e1a614fd4d7f001894692168e0d7929e
beate <beate@infokennel.ch>
2023-06-22 10:32:16 +0000

    element.php: Don't "unNumberFormat" null values https://fabrikar.com/forums/index.php?threads/decimal-fields.53921/

--- components/com_fabrik/models/element.php
+++ components/com_fabrik/models/element.php
@@ -7088,7 +7088,7 @@ class PlgFabrik_Element extends FabrikPlugin
 	{
 		$params = $this->getParams();
 
-		if (!$params->get('field_use_number_format', false))
+		if (!$params->get('field_use_number_format', false) || is_null($val))
 		{
 			return $val;
 		}
