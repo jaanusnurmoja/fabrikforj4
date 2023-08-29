c765cee638527c621df4fe25fbae99f6ed9fa36d
beate <beate@infokennel.ch>
2023-08-29 10:59:52 +0000

    birthday.php: fix nasty error introduced while fixing an other one.

--- plugins/fabrik_element/birthday/birthday.php
+++ plugins/fabrik_element/birthday/birthday.php
@@ -420,20 +420,15 @@ class PlgFabrik_ElementBirthday extends PlgFabrik_Element
 
 		if (is_array($val))
 		{
-			if ($params->get('empty_is_null', '1') == 0 || !in_array('', $val))
-			{
-				return $val[2] . '-' . $val[1] . '-' . $val[0];
-			}
+			$val = $val[2] . '-' . $val[1] . '-' . $val[0];
 		}
-		else
-		{
-			if ($params->get('empty_is_null', '1') == '0') {
-				if (!in_array('', explode('-',$val??''))) return $val;
-				else  return '0000-00-00';
-			}
+		
+		if (!in_array('', explode('-',$val??''))) {
+			//complete birthday
+			return $val;
 		}
-
-		return;
+		elseif ($params->get('empty_is_null', '1') == 0) return '0000-00-00';
+		else return null;
 	}
 
 	/**
