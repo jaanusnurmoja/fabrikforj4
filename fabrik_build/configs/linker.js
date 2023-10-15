25d70044c11b3d86fd02d80b7b65d544f6268972
Alex Chartier <achartier@admiralsecure.com>
2023-10-15 10:15:57 -0400

    Formatting for clarity.

--- fabrik_build/configs/linker.js
+++ fabrik_build/configs/linker.js
@@ -1,28 +1,28 @@
 module.exports = {
 	'targetDirectories' : [
-        'administrator/components/com_fabrik',
-        'components/com_fabrik',
-        'plugins/system/fabrik',
-        'plugins/content/fabrik',
-        'media/com_fabrik'
-		],
+	        'administrator/components/com_fabrik',
+	        'components/com_fabrik',
+	        'plugins/system/fabrik',
+	        'plugins/content/fabrik',
+	        'media/com_fabrik'
+	],
 	'targetSubDirectories' : [
-        'administrator/modules',
-        'modules'
-		],
+		        'administrator/modules',
+		        'modules'
+	],
 	'targetPluginTypes' : {
-        'plugins/community/fabrik' : [],
-        'plugins/fabrik_cron' : ['email', 'php'],
-        'plugins/fabrik_element' : ['access', 'birthday', 'button', 'calc', 'captcha', 'cascadingdropdown', 'checkbox', 'colourpicker',
+	        'plugins/community/fabrik' : [],
+	        'plugins/fabrik_cron' : ['email', 'php'],
+	        'plugins/fabrik_element' : ['access', 'birthday', 'button', 'calc', 'captcha', 'cascadingdropdown', 'checkbox', 'colourpicker',
         		'databasejoin', 'date', 'digsig', 'display', 'dropdown', 'field', 'fileupload', 'folder', 'googlemap', 'image', 
         		'internalid', 'ip', 'jdate', 'link', 'lockrow', 'password', 'picklist', 'radiobutton', 'sequence', 'slider',
-                'textarea', 'thumbs', 'time', 'timer', 'timestamp', 'total', 'user', 'usergroup', 'viewlevel', 'yesno'],
-        'plugins/fabrik_form' : ['autofill', 'comment', 'email', 'juser', 'php', 'receipt', 'redirect'],
-        'plugins/fabrik_list' : ['candeleterow', 'caneditrow', 'canviewrow', 'copy', 'js', 'link', 'lockrow', 'order', 'php', 'update_col'],
-        'plugins/fabrik_validationrule' : ['areuniquevalues', 'emailexists', 'isalphanumeric', 'isemail', 'isgreaterorlessthan',
-                'isiban', 'isnot', 'isnumeric', 'isuniquevalue', 'notempty', 'php'],
-        'plugins/fabrik_visualization' : ['fullcalendar', 'googlemap']
-		},
+                	'textarea', 'thumbs', 'time', 'timer', 'timestamp', 'total', 'user', 'usergroup', 'viewlevel', 'yesno'],
+	        'plugins/fabrik_form' : ['autofill', 'comment', 'email', 'juser', 'php', 'receipt', 'redirect'],
+	        'plugins/fabrik_list' : ['candeleterow', 'caneditrow', 'canviewrow', 'copy', 'js', 'link', 'lockrow', 'order', 'php', 'update_col'],
+	        'plugins/fabrik_validationrule' : ['areuniquevalues', 'emailexists', 'isalphanumeric', 'isemail', 'isgreaterorlessthan',
+	                'isiban', 'isnot', 'isnumeric', 'isuniquevalue', 'notempty', 'php'],
+	        'plugins/fabrik_visualization' : ['fullcalendar', 'googlemap']
+	},
 	'targetLibraries' : {
 		'fabrik' : {'fabrik.xml' : 'fabrik.xml', 'include.php' : 'include.php'}, 
 		'vendor' : {'vendor.xml' : 'vendor.xml'}, 
