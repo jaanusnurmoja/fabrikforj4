3509be9a49acdb0941894699d98ed34330f8f38f
Alex Chartier <achartier@admiralsecure.com>
2023-10-17 14:00:30 -0400

    Fix the sorting functions for the admin.

--- administrator/components/com_fabrik/views/crons/tmpl/bootstrap.php
+++ administrator/components/com_fabrik/views/crons/tmpl/bootstrap.php
@@ -36,44 +36,44 @@ $imgs = array('publish_x.png', 'tick.png', 'publish_y.png');
 $tasks = array('publish', 'unpublish', 'publish');
 
 ?>
-<form action="<?php echo Route::_('index.php?option=com_fabrik&view=crons'); ?>" method="post" name="adminForm" id="adminForm">
+<form action="<?= Route::_('index.php?option=com_fabrik&view=crons'); ?>" method="post" name="adminForm" id="adminForm">
 <div class="row">
 <div class="col-sm-12">
 	<div id="j-main-container" class="j-main-container">
-		<?php echo LayoutHelper::render('joomla.searchtools.default', array('view' => $this)); ?>
+		<?= LayoutHelper::render('joomla.searchtools.default', array('view' => $this)); ?>
 		<?php if (empty($this->items)) : ?>
 			<div class="alert alert-info">
-				<span class="icon-info-circle" aria-hidden="true"></span><span class="visually-hidden"><?php echo Text::_('INFO'); ?></span>
-				<?php echo Text::_('JGLOBAL_NO_MATCHING_RESULTS'); ?>
+				<span class="icon-info-circle" aria-hidden="true"></span><span class="visually-hidden"><?= Text::_('INFO'); ?></span>
+				<?= Text::_('JGLOBAL_NO_MATCHING_RESULTS'); ?>
 			</div>
 		<?php else : ?>
 	<table class="table table-striped">
 		<thead>
 			<tr>
-				<th width="2%">
-					<?php echo HTMLHelper::_('grid.sort', 'JGRID_HEADING_ID', 'c.id', $listDirn, $listOrder); ?>
+				<td class="w-1 text-center">
+					<?= HTMLHelper::_('grid.checkall'); ?>
+				</td>
+				<th scope="col" class="w-1 text-center d-none d-md-table-cell">
+                    <?= HTMLHelper::_('searchtools.sort', 'JGRID_HEADING_ID', 'c.id', $listDirn, $listOrder); ?>
 				</th>
-				<th width="1%">
-					<?php echo HTMLHelper::_('grid.checkall'); ?>
+                <th scope="col">
+                    <?= HTMLHelper::_('searchtools.sort', 'COM_FABRIK_LABEL', 'c.label', $listDirn, $listOrder); ?>
 				</th>
-				<th width="60%">
-					<?php echo HTMLHelper::_('grid.sort', 'COM_FABRIK_LABEL', 'c.label', $listDirn, $listOrder); ?>
+                <th scope="col">
+                    <?= HTMLHelper::_('searchtools.sort', 'COM_FABRIK_CRON_FREQUENCY', 'c.frequency', $listDirn, $listOrder); ?>
 				</th>
-				<th width="20%">
-					<?php echo HTMLHelper::_('grid.sort', 'COM_FABRIK_CRON_FREQUENCY', 'c.frequency'.'/'.'c.unit', $listDirn, $listOrder); ?>
+                <th scope="col">
+                    <?= HTMLHelper::_('searchtools.sort', 'COM_FABRIK_CRON_FIELD_LAST_RUN_LABEL', 'c.lastrun', $listDirn, $listOrder); ?>
 				</th>
-				<th width="20%">
-					<?php echo HTMLHelper::_('grid.sort', 'COM_FABRIK_CRON_FIELD_LAST_RUN_LABEL', 'c.lastrun', $listDirn, $listOrder); ?>
-				</th>
-				<th width="5%">
-					<?php echo HTMLHelper::_('grid.sort', 'JPUBLISHED', 'c.published', $listDirn, $listOrder); ?>
+				<th scope="col" class="w-3 d-none d-md-table-cell">
+                    <?= HTMLHelper::_('searchtools.sort', 'JPUBLISHED', 'c.published', $listDirn, $listOrder); ?>
 				</th>
 			</tr>
 		</thead>
 		<tfoot>
 			<tr>
 				<td colspan="5">
-					<?php echo $this->pagination->getListFooter(); ?>
+					<?= $this->pagination->getListFooter(); ?>
 				</td>
 			</tr>
 		</tfoot>
@@ -85,35 +85,33 @@ $tasks = array('publish', 'unpublish', 'publish');
 	$canChange = true;
 			   ?>
 
-			<tr class="row<?php echo $i % 2; ?>">
+			<tr class="row<?= $i % 2; ?>">
+					<td><?= HTMLHelper::_('grid.id', $i, $item->id); ?></td>
 					<td>
-						<?php echo $item->id; ?>
-					</td>
-					<td>
-						<?php echo HTMLHelper::_('grid.id', $i, $item->id); ?>
+						<?= $item->id; ?>
 					</td>
 					<td>
 						<?php if ($item->checked_out) : ?>
-							<?php echo HTMLHelper::_('jgrid.checkedout', $i, $item->editor, $item->checked_out_time, 'crons.', $canCheckin); ?>
+							<?= HTMLHelper::_('jgrid.checkedout', $i, $item->editor, $item->checked_out_time, 'crons.', $canCheckin); ?>
 						<?php endif; ?>
 						<?php
 						if ($item->checked_out && ($item->checked_out != $user->get('id'))) :
 							echo $item->label;
 						else:
 						?>
-						<a href="<?php echo $link; ?>">
-							<?php echo $item->label; ?>
+						<a href="<?= $link; ?>">
+							<?= $item->label; ?>
 						</a>
 					<?php endif; ?>
 					</td>
 					<td>
-						<?php echo $item->frequency .' '. $item->unit; ?>
+						<?= $item->frequency .' '. $item->unit; ?>
 					</td>
 					<td>
-						<?php echo HTMLHelper::_('date', $item->lastrun, 'Y-m-d H:i:s'); ?>
+						<?= HTMLHelper::_('date', $item->lastrun, 'Y-m-d H:i:s'); ?>
 					</td>
-					<td class="center">
-						<?php echo HTMLHelper::_('jgrid.published', $item->published, $i, 'crons.', $canChange);?>
+					<td class="text-center">
+						<?= HTMLHelper::_('jgrid.published', $item->published, $i, 'crons.', $canChange);?>
 					</td>
 				</tr>
 
@@ -124,8 +122,6 @@ $tasks = array('publish', 'unpublish', 'publish');
 
 	<input type="hidden" name="task" value="" />
 	<input type="hidden" name="boxchecked" value="0" />
-	<input type="hidden" name="filter_order" value="<?php echo $listOrder; ?>" />
-	<input type="hidden" name="filter_order_Dir" value="<?php echo $listDirn; ?>" />
-	<?php echo HTMLHelper::_('form.token'); ?>
+	<?= HTMLHelper::_('form.token'); ?>
 	</div>
 </form>
--- administrator/components/com_fabrik/views/forms/tmpl/bootstrap.php
+++ administrator/components/com_fabrik/views/forms/tmpl/bootstrap.php
@@ -36,51 +36,51 @@ $userId    = $user->get('id');
 $listOrder = $this->state->get('list.ordering');
 $listDirn  = $this->state->get('list.direction');
 ?>
-<form action="<?php echo Route::_('index.php?option=com_fabrik&view=forms'); ?>" method="post" name="adminForm" id="adminForm">
+<form action="<?= Route::_('index.php?option=com_fabrik&view=forms'); ?>" method="post" name="adminForm" id="adminForm">
 <div class="row">
 <div class="col-sm-12">
 	<div id="j-main-container" class="j-main-container">
-		<?php echo LayoutHelper::render('joomla.searchtools.default', array('view' => $this)); ?>
+		<?= LayoutHelper::render('joomla.searchtools.default', array('view' => $this)); ?>
 			<?php if (empty($this->items)) : ?>
 				<div class="alert alert-info">
-					<span class="icon-info-circle" aria-hidden="true"></span><span class="visually-hidden"><?php echo Text::_('INFO'); ?></span>
-					<?php echo Text::_('JGLOBAL_NO_MATCHING_RESULTS'); ?>
+					<span class="icon-info-circle" aria-hidden="true"></span><span class="visually-hidden"><?= Text::_('INFO'); ?></span>
+					<?= Text::_('JGLOBAL_NO_MATCHING_RESULTS'); ?>
 				</div>
 			<?php else : ?>
 
 			<table class="table table-striped">
 				<thead>
 				<tr>
-					<th width="2%">
-						<?php echo HTMLHelper::_('grid.sort', '#', 'f.id', $listDirn, $listOrder); ?>
+					<td class="w-1 text-center">
+						<?= HTMLHelper::_('grid.checkall'); ?>
+					</td>
+					<th scope="col" class="w-1 text-center d-none d-md-table-cell">
+                        <?= HTMLHelper::_('searchtools.sort', 'JGRID_HEADING_ID', 'f.id', $listDirn, $listOrder); ?>
 					</th>
-					<th width="1%">
-						<?php echo HTMLHelper::_('grid.checkall'); ?>
+                    <th scope="col">
+                        <?= HTMLHelper::_('searchtools.sort', 'COM_FABRIK_LABEL', 'f.label', $listDirn, $listOrder); ?>
 					</th>
-					<th width="40%">
-						<?php echo HTMLHelper::_('grid.sort', 'COM_FABRIK_LABEL', 'f.label', $listDirn, $listOrder); ?>
+					<th scope="col" class="w-10 d-none d-md-table-cell">
+						<?= Text::_('COM_FABRIK_ELEMENT'); ?>
 					</th>
-					<th width="10%">
-						<?php echo Text::_('COM_FABRIK_ELEMENT'); ?>
+					<th scope="col" class="w-10 d-none d-md-table-cell">
+						<?= Text::_('COM_FABRIK_CONTENT_TYPE'); ?>
 					</th>
-					<th width="5%">
-						<?php echo Text::_('COM_FABRIK_CONTENT_TYPE'); ?>
+					<th scope="col" class="w-10 d-none d-md-table-cell">
+						<?= Text::_('COM_FABRIK_UPDATE_DATABASE'); ?>
 					</th>
-					<th width="20%">
-						<?php echo Text::_('COM_FABRIK_UPDATE_DATABASE'); ?>
+					<th scope="col" class="w-10 d-none d-md-table-cell">
+						<?= Text::_('COM_FABRIK_VIEW_DATA'); ?>
 					</th>
-					<th width="15%">
-						<?php echo Text::_('COM_FABRIK_VIEW_DATA'); ?>
-					</th>
-					<th width="5%">
-						<?php echo HTMLHelper::_('grid.sort', 'JPUBLISHED', 'f.published', $listDirn, $listOrder); ?>
+					<th scope="col" class="w-3 d-none d-md-table-cell">
+                        <?= HTMLHelper::_('searchtools.sort', 'JPUBLISHED', 'f.published', $listDirn, $listOrder); ?>
 					</th>
 				</tr>
 				</thead>
 				<tfoot>
 				<tr>
 					<td colspan="6">
-						<?php echo $this->pagination->getListFooter(); ?>
+						<?= $this->pagination->getListFooter(); ?>
 					</td>
 				</tr>
 				</tfoot>
@@ -96,33 +96,33 @@ $listDirn  = $this->state->get('list.direction');
 
 					$elementLink = Route::_('index.php?option=com_fabrik&task=element.edit&id=0&filter_groupId=' . $item->group_id);
 					?>
-					<tr class="row<?php echo $i % 2; ?>">
-						<td><?php echo $item->id; ?></td>
-						<td><?php echo HTMLHelper::_('grid.id', $i, $item->id); ?></td>
+					<tr class="row<?= $i % 2; ?>">
+						<td><?= HTMLHelper::_('grid.id', $i, $item->id); ?></td>
+						<td><?= $item->id; ?></td>
 						<td>
 							<?php if ($item->checked_out) : ?>
-								<?php echo HTMLHelper::_('jgrid.checkedout', $i, $item->editor, $item->checked_out_time, 'forms.', $canCheckin); ?>
+								<?= HTMLHelper::_('jgrid.checkedout', $i, $item->editor, $item->checked_out_time, 'forms.', $canCheckin); ?>
 							<?php endif; ?>
 							<?php
 							if ($item->checked_out && ($item->checked_out != $user->get('id')))
 							{ ?>
 								<span class="editlinktip hasTip"
-									title="foo <?php echo Text::_($item->label) . "::" . $params->get('note'); ?>"> <?php echo Text::_($item->label); ?>
+									title="foo <?= Text::_($item->label) . "::" . $params->get('note'); ?>"> <?= Text::_($item->label); ?>
 								</span>
 							<?php }
 							else
 							{
 								?>
-								<a href="<?php echo $link; ?>">
-									<span class="editlinktip hasTip" title="<?php echo Text::_($item->label) . "::" . $params->get('note'); ?>">
-										<?php echo Text::_($item->label); ?>
+								<a href="<?= $link; ?>">
+									<span class="editlinktip hasTip" title="<?= Text::_($item->label) . "::" . $params->get('note'); ?>">
+										<?= Text::_($item->label); ?>
 									</span>
 								</a>
 							<?php } ?>
 						</td>
 						<td>
-							<a href="<?php echo $elementLink ?>">
-								<i class="icon-plus"></i> <?php echo Text::_('COM_FABRIK_ADD'); ?>
+							<a href="<?= $elementLink ?>">
+								<i class="icon-plus"></i> <?= Text::_('COM_FABRIK_ADD'); ?>
 							</a>
 						</td>
 						<td>
@@ -134,15 +134,15 @@ $listDirn  = $this->state->get('list.direction');
 								</button>
 								<ul class="dropdown-menu">
 									<li>
-										<a href="javascript://" onclick="Joomla.listItemTask('cb<?php echo $i; ?>', 'form.createContentType')">
-											<span class="icon-upload text-nowrap"> <?php echo Text::_('COM_FABRIK_CONTENT_TYPE_EXPORT'); ?></span>
+										<a href="javascript://" onclick="Joomla.listItemTask('cb<?= $i; ?>', 'form.createContentType')">
+											<span class="icon-upload text-nowrap"> <?= Text::_('COM_FABRIK_CONTENT_TYPE_EXPORT'); ?></span>
 										</a>
 									</li>
 									<?php
 									if ($params->get('content_type_path', '') !== '') :?>
 										<li>
-											<a href="index.php?option=com_fabrik&task=form.downloadContentType&cid=<?php echo $item->id; ?>">
-												<span class="icon-download text-nowrap"> <?php echo Text::_('COM_FABRIK_CONTENT_TYPE_DOWNLOAD'); ?></span>
+											<a href="index.php?option=com_fabrik&task=form.downloadContentType&cid=<?= $item->id; ?>">
+												<span class="icon-download text-nowrap"> <?= Text::_('COM_FABRIK_CONTENT_TYPE_DOWNLOAD'); ?></span>
 											</a>
 										</li>
 										<?php
@@ -153,17 +153,17 @@ $listDirn  = $this->state->get('list.direction');
 							</div>
 						</td>
 						<td>
-							<a href="#edit" onclick="return Joomla.listItemTask('cb<?php echo $i; ?>','forms.updateDatabase')">
-								<i class="icon-refresh"></i> <?php echo Text::_('COM_FABRIK_UPDATE_DATABASE'); ?>
+							<a href="#edit" onclick="return Joomla.listItemTask('cb<?= $i; ?>','forms.updateDatabase')">
+								<i class="icon-refresh"></i> <?= Text::_('COM_FABRIK_UPDATE_DATABASE'); ?>
 							</a>
 						</td>
 						<td>
-							<a href="index.php?option=com_fabrik&task=list.view&listid=<?php echo $item->list_id ?>">
-								<i class="icon-list-view"></i> <?php echo Text::_('COM_FABRIK_VIEW_DATA'); ?>
+							<a href="index.php?option=com_fabrik&task=list.view&listid=<?= $item->list_id ?>">
+								<i class="icon-list-view"></i> <?= Text::_('COM_FABRIK_VIEW_DATA'); ?>
 							</a>
 						</td>
-						<td class="center">
-							<?php echo HTMLHelper::_('jgrid.published', $item->published, $i, 'forms.', $canChange); ?>
+						<td class="text-center">
+							<?= HTMLHelper::_('jgrid.published', $item->published, $i, 'forms.', $canChange); ?>
 						</td>
 					</tr>
 
@@ -174,8 +174,6 @@ $listDirn  = $this->state->get('list.direction');
 
 			<input type="hidden" name="task" value="" />
 			<input type="hidden" name="boxchecked" value="0" />
-			<input type="hidden" name="filter_order" value="<?php echo $listOrder; ?>" />
-			<input type="hidden" name="filter_order_Dir" value="<?php echo $listDirn; ?>" />
-			<?php echo HTMLHelper::_('form.token'); ?>
+			<?= HTMLHelper::_('form.token'); ?>
 		</div>
 </form>
