be6b2e65d3b1db079bf488eea744df4a9baee0aa
Alex Chartier <achartier@admiralsecure.com>
2024-01-27 12:56:44 -0500

    purged

--- media/com_fabrik/js/form.js
+++ media/com_fabrik/js/form.js
@@ -2493,8 +2493,13 @@ define(['jquery', 'fab/encoder', 'fab/fabrik', 'lib/debounce/jquery.ba-throttle-
                             // $$$ hugh - sanity check in case we have an element which has no input
                             if (document.id(testid).getElement('input')) {
                                 input.cloneEvents(document.id(testid).getElement('input'));
+                                // Note: Radio's etc. now have their events delegated from the form - so no need to duplicate them
+                                // Update labels for sub elements
+                                var l = subElementContainer.getParent('.fabrikElementContainer').getElement('label');
+                                if (l) {
+                                    l.setProperty('for', subElementContainer.id);
+                               }
                             }
-                            // Note: Radio's etc. now have their events delegated from the form - so no need to duplicate them
 
                         } else {
                             input.cloneEvents(el.element);
@@ -2546,6 +2551,25 @@ define(['jquery', 'fab/encoder', 'fab/fabrik', 'lib/debounce/jquery.ba-throttle-
                         newEl.cloneUpdateIds(subElementContainer.id);
                         newEl.options.element = subElementContainer.id;
                         newEl._getSubElements();
+                        // update the labels
+                        newEl.subElements.each(function (subEl) {
+                            l = subEl.nextElementSibling;
+                            while (l) {
+                                if (l.nodeName == "LABEL") {
+                                    var lBits = l.htmlFor.split('_');
+                                    var eBits = newEl.options.element.split('_');
+                                    lBits[6] = eBits[6];
+                                    l.htmlFor = lBits.join('_');
+                                    break;
+                                }
+                                l = l.nextElementSibling;
+                            }
+                        });
+                        // Update the id on the fieldset if there is one
+                        var fs = subElementContainer.getElement('fieldset');
+                        if (fs) {
+                            fs.setAttribute('id', subElementContainer.id);
+                        }
                     } else {
                         newEl.cloneUpdateIds(lastinput.id);
                     }
@@ -2576,6 +2600,10 @@ define(['jquery', 'fab/encoder', 'fab/fabrik', 'lib/debounce/jquery.ba-throttle-
                     newEl.resetEvents();
                 }
             }.bind(this));
+            
+            // Update the element container label
+            lastinput.getParent('.fabrikElementContainer').getElement('label').htmlFor = container.id;
+
             var o = {};
             o[i] = newElementControllers;
             this.addElements(o);
