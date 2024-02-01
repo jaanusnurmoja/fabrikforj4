1d1821e3c481c0fcf6b23bebe188257bad55565c
Alex Chartier <achartier@admiralsecure.com>
2024-02-01 06:12:20 -0500

    V4.0 Official

--- administrator/components/com_fabrik/views/list/tmpl/adminlist-min.js
+++ administrator/components/com_fabrik/views/list/tmpl/adminlist-min.js
@@ -1,3 +1,2 @@
 /*! Fabrik */
-
 define(["jquery","admin/pluginmanager"],function(n,i){return new Class({Extends:i,type:"list",initialize:function(n,i){this.parent(n,i)}})});
--- plugins/fabrik_element/count/count-min.js
+++ plugins/fabrik_element/count/count-min.js
@@ -1,3 +1,2 @@
 /*! Fabrik */
-
 define(["jquery","fab/element"],function(n,i){return window.FbCount=new Class({Extends:i,initialize:function(n,i){this.setPlugin("fabrikcount"),this.parent(n,i)}}),window.FbCount});
