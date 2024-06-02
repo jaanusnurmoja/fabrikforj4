1e4db63ad5faffff58c1bcc0b33c4e9b36af3b36
beate <beate@infokennel.ch>
2024-06-02 06:54:05 +0000

    php.php: add missing variables to phpeval https://fabrikar.com/forums/index.php?threads/formmodel-not-available-in-php-element-validation-plugin.54556

--- plugins/fabrik_validationrule/php/php.php
+++ plugins/fabrik_validationrule/php/php.php
@@ -103,7 +103,7 @@ class PlgFabrik_ValidationrulePhp extends PlgFabrik_Validationrule {
 		 * $$$ hugh - moved the $trigger_error() into a helper func
 		 */
 		FabrikWorker::clearEval();
-		$return = Php::Eval(['code' => $phpCode, 'vars' => ['thisValidation' => $this, 'data' => $data, 'formData' => $formData]]);
+		$return = Php::Eval(['code' => $phpCode, 'vars' => ['thisValidation' => $this, 'data' => $data, 'formData' => $formData,'formModel'=>$formModel,'repeatCounter'=>$repeatCounter,'elementModel'=>$elementModel]]);
 		FabrikWorker::logEval($return, 'Caught exception on php validation of ' . $elementModel->getFullName(true, false) . ': %s');
 
 		return $return;
