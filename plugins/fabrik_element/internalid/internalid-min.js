9b0c8744258ec3b654021fac02cb80d3c779c125
Alex Chartier <achartier@admiralsecure.com>
2024-09-02 06:30:43 -0400

    Revised for WAM. Moved media

--- plugins/fabrik_element/internalid/internalid-min.js
+++
@@ -1,2 +0,0 @@
-/*! Fabrik */
-define(["jquery","fab/element"],function(n,e){return window.FbInternalId=new Class({Extends:e,initialize:function(n,e){this.setPlugin("fbInternalId"),this.parent(n,e)}}),window.FbInternalId});
