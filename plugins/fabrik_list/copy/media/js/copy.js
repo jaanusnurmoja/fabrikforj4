712f6266515d229a91ae4a4b1869fd93ad934759
Alex Chartier <achartier@admiralsecure.com>
2024-09-03 07:03:10 -0400

    New media files for these plugins

---
+++ plugins/fabrik_list/copy/media/js/copy.js
@@ -0,0 +1,12 @@
+/**
+ * List Copy
+ *
+ * @copyright: Copyright (C) 2005-2016  Media A-Team, Inc. - All rights reserved.
+ * @license:   GNU/GPL http://www.gnu.org/copyleft/gpl.html
+ */
+var FbListCopy = new Class({
+	Extends   : FbListPlugin,
+	initialize: function (options) {
+		this.parent(options);
+	}
+});
