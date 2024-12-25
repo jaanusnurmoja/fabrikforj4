f40355c87ff39510d577f415dbb570a50cbf18f5
Alex Chartier <achartier@admiralsecure.com>
2024-12-25 08:44:29 -0500

    ES6

--- plugins/fabrik_element/access/media/js/access.js
+++ plugins/fabrik_element/access/media/js/access.js
@@ -5,12 +5,13 @@
  * @license:   GNU/GPL http://www.gnu.org/copyleft/gpl.html
  */
 
-window.FbAccess = new Class({
+import { FbElement } from "@element"; 
 
-    Extends: FbElement,
+export class FbAccess extends FbElement{
+	constructor (element, options) {
+		super(element, options);
+		this.setPlugin('fbaccess');
+	}
+}
 
-    initialize: function (element, options) {
-        this.setPlugin('fabrikaccess');
-        this.parent(element, options);
-    }
-});
+window.FbAccess = FbAccess;
