f1274c868481e038b495ce0efbd29ef88bff8620
Alex Chartier <achartier@admiralsecure.com>
2025-01-05 15:24:30 -0500

    Revised for F5

---
+++ plugins/fabrik_validationrule/notempty/services/provider.php
@@ -0,0 +1,32 @@
+<?php
+defined('_JEXEC') || die;
+
+use Joomla\CMS\Extension\PluginInterface;
+use Joomla\CMS\Factory;
+use Joomla\CMS\Plugin\PluginHelper;
+use Joomla\DI\Container;
+use Joomla\DI\ServiceProviderInterface;
+use Joomla\Event\DispatcherInterface;
+use Fabrik\Plugin\Validationrule\Notempty\Extension\Notempty;
+
+return new class implements ServiceProviderInterface {
+    public function register(Container $container)
+    {
+        $container->set(
+            PluginInterface::class,
+            function (Container $container)
+            {
+                $config  = (array)PluginHelper::getPlugin('fabrik_validationrule', 'Notempty');
+                $subject = $container->get(DispatcherInterface::class);
+
+                $app = Factory::getApplication();
+
+                /** @var \Joomla\CMS\Plugin\CMSPlugin $plugin */
+                $plugin = new Notempty($subject, $config);
+                $plugin->setApplication($app);
+
+                return $plugin;
+            }
+        );
+    }
+};
---
+++ plugins/fabrik_validationrule/notempty/src/Extension/Notempty.php
@@ -0,0 +1,84 @@
+<?php
+/**
+ * Not Empty Validation Rule
+ *
+ * @package     Joomla.Plugin
+ * @subpackage  Fabrik.validationrule.notempty
+ * @copyright   Copyright (C) 2005-2020  Media A-Team, Inc. - All rights reserved.
+ * @license     GNU/GPL http://www.gnu.org/copyleft/gpl.html
+ */
+
+namespace Fabrik\Plugin\Validationrule\Notempty\Extension;
+
+// No direct access
+defined('_JEXEC') or die('Restricted access');
+
+use Fabrik\Component\Fabrik\Site\Model\PluginvalidationruleModel;
+use Joomla\Event\SubscriberInterface;
+
+/**
+ * Not Empty Validation Rule
+ *
+ * @package     Joomla.Plugin
+ * @subpackage  Fabrik.validationrule.notempty
+ * @since       3.0
+ */
+class Notempty extends PluginvalidationruleModel implements SubscriberInterface
+{
+	protected $app; // Provided by the CSMPlugin interface
+
+	/**
+	 * Plugin name
+	 *
+	 * @var string
+	 */
+	protected $pluginName = 'notempty';
+
+	/**
+     * Returns an array of events this subscriber will listen to.
+     *
+     * @return  array
+     *
+     * @since   5.0
+     */
+    public static function getSubscribedEvents(): array
+    {
+        $pluginMethods = [];
+
+        return array_merge(parent::getSubscribedEvents(), $pluginMethods);
+    }
+
+	/**
+	 * Returns the javascript import map name for the plugin javascript.
+	 *
+	 * @return  string
+	 *
+	 * @since   5.0
+	 */
+	public function getImportMapName()
+	{
+		return 'import { FbValidationrulenotempty } from "@fbvalidationrulenotempty";';
+	}
+
+	/**
+	 * Validate the elements data against the rule
+	 *
+	 * @param   string  $data           To check
+	 * @param   int     $repeatCounter  Repeat group counter
+	 *
+	 * @return  bool  true if validation passes, false if fails
+	 */
+	public function validate($data, $repeatCounter)
+	{
+		if (method_exists($this->elementModel, 'dataConsideredEmptyForValidation'))
+		{
+			$ok = $this->elementModel->dataConsideredEmptyForValidation($data, $repeatCounter);
+		}
+		else
+		{
+			$ok = $this->elementModel->dataConsideredEmpty($data, $repeatCounter);
+		}
+
+		return !$ok;
+	}
+}
